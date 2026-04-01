1db05e0261b586a753b7e9b927d1820f46d07fb2
abdosaad203 <abdosaad4900@gmail.com>
2026-04-01 04:31:57 +0200

    update user_data to deploy website

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 dnf update -y
-dnf install -y nginx
+dnf install -y nginx unzip
+
+cd /tmp
+wget https://www.tooplate.com/zip-templates/2134_gotto_job.zip
+unzip 2134_gotto_job.zip
+
+cp -r 2134_gotto_job/* /usr/share/nginx/html/
+
 systemctl start nginx
 systemctl enable nginx
-echo "Hello from EC2 $(hostname)" > /usr/share/nginx/html/index.html
